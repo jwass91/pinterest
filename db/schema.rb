e533d6bde24fef3348e2f656ac6f7d2b5a8ec73f
jackson987 <robbins.jackson1@gmail.com>
2015-08-06 13:48:34 +0000

    oops

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805172404) do
+ActiveRecord::Schema.define(version: 20150806134506) do
 
   create_table "pins", force: :cascade do |t|
     t.string "user"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20150805172404) do
     t.string "link"
     t.string "description"
     t.string "category"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "usernmae"
+    t.string "email"
+    t.string "fullname"
   end
 
 end
