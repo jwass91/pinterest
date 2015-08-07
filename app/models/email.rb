c11a974182084838bcc88521fcb9ee321a34b2e0
Jared Wasserman <jwass91@gmail.com>
2015-08-07 18:17:34 +0000

    added verfiy

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,12 +1,12 @@
 class Email 
   
-def send_simple_message(email, username)
+  def send_simple_message(email, username, key)
   RestClient.post "https://api:key-c75265d73687fc46ca0bdf558ee5b41a"\
   "@api.mailgun.net/v3/sandbox8bc08204b98243e5be44eb95bcbdb14e.mailgun.org/messages",
   :from => "Mailgun Sandbox <postmaster@sandbox8bc08204b98243e5be44eb95bcbdb14e.mailgun.org>",
   :to => email,
   :subject => "Hi #{username}, thanks for signing up. Verify your email for pintrest!",
-  :text => "Hi #{username}. Please click this link to verify your email: "
+    :text => "Hi #{username}. Please click this link to verify your email: #{key} "
 end
   
   
